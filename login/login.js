ee7aa29923383a6aebd0d4dfbf7ebf41d69acc18
Isabella Patrício <125597180+IsabellaPaty@users.noreply.github.com>
2023-09-28 12:35:18 -0300

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,3 +1,17 @@
-exports.myDateTime = function () {
-    return "login";
-  };
+const express = require('express');
+const app = express();
+const port = 8000; // Porta em que o servidor será executado
+// Rota para exibir a página HTML
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/login.html');
+
+});
+app.get('/login.css', (req, res) => {
+res.sendFile(__dirname + '/login.css');
+
+});
+// Iniciar o servidor
+app.listen(port, () => {
+console.log(`Servidor Express está rodando na porta ${port}`);
+});
+
